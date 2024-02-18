85c3f65d106735f8563a59c8812c7ac679d1ef35
vuvietnam <144465758+serpent1neX@users.noreply.github.com>
2024-02-18 22:16:21 +0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+[BepInPlugin("com.example.UniformADS", "Uniform ADS Plugin", "1.0.0")]
+public class UniformADSPlugin : BaseUnityPlugin
+{
+    void Awake()
+    {
+        // Use the specific version of Harmony that works with your project
+        HarmonyLib.Harmony harmony = new HarmonyLib.Harmony("com.example.UniformADS");
+        harmony.PatchAll();
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(YourClass), "YourMethod")]
+    [HarmonyLib.HarmonyPostfix]
+    private static void YourMethodPostfix()
+    {
+        // Your patch logic
+    }
+}
---
+++ YourPatchClass.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace UniformADS
+{
+    [HarmonyPatch(typeof(YourClass))] // Replace YourClass with the actual class you are targeting
+    [HarmonyPatch("YourMethod")] // Replace YourMethod with the actual method you are targeting
+    class YourPatchClass
+    {
+        [HarmonyPostfix]
+        static void Postfix()
+        {
+            // Your patch code here
+            UnityEngine.Debug.Log("Patch applied!"); // Example log message
+        }
+    }
+}
